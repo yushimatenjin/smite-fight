485d9af86ccc4fc28794b206ab3d2b5f3977bb07
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2021-01-25 00:24:43 +0900

    initial commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1076401.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 41518195, 41518196, 41518210, 41518264, 41518335, 41518406, 41518581, 41518584, 41518591, 41518633, 41519034, 41519244, 41522181, 41522573 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
